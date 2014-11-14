ae25a86262ea3bdb4571d55d3a4bd6749b0879a1
Yosi Attias <yosy101@gmail.com>
2014-11-14 07:49:00 +0200

    Removing wildcards

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ LevEhad::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  config.assets.precompile += %w( allocation.js allocation.css allocation_approval.js volunteers.css *.css *.js )
+  config.assets.precompile += %w( allocation.js allocation.css allocation_approval.js volunteers.css )
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
