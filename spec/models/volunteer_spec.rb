eb547471c158981266c2eb09bbadc713dd430193
Omer Levi Hevroni <omerlh@gmail.com>
2014-11-18 22:46:44 +0200

    add factory for volunteer

--- spec/models/volunteer_spec.rb
+++ spec/models/volunteer_spec.rb
@@ -1,7 +1,9 @@
 require 'spec_helper' 
 
 describe Volunteer do 
-  it "has a valid factory" 
+  it "has a valid factory" do 
+  	Factory.create(:contact).should be_valid 
+  end
   it "is invalid without a firstname" 
   it "is invalid without a lastname" 
   it "returns a Volunteer's full name as a string" 
